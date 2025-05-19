015dbe449fb27d77963e08e6a5fd7bf37236c5f1
Parmar.Unnati <Parmar.Unnati@WIUNNATI>
2025-05-19 15:57:51 +0530

    19 05 2025

--- AlertSystem/Controllers/HomeController.cs
+++ AlertSystem/Controllers/HomeController.cs
@@ -104,81 +104,21 @@ namespace AlertSystem.Controllers
                 {
                     try
                     {
-                        result = getSystem.Select((GetSystwmModel part) =>
-                        {
-                            TransactionSummary trans = transactionDetails
-                                .FirstOrDefault((TransactionSummary t) => t.PartNum == part.PartNum);
-                            int onHandQty = onHandQtyDict.ContainsKey(part.PartNum) ? onHandQtyDict[part.PartNum] : 0;
-                            double monthsInStock = (part.Yearly_Qty.GetValueOrDefault() > 0) ? onHandQty / ((double)part.Yearly_Qty.GetValueOrDefault() / 12) : 0;
-                            int rank = monthsInStock >= 12 ? 5 :
-                                       monthsInStock >= 9 ? 4 :
-                                       monthsInStock >= 6 ? 3 :
-                                       monthsInStock >= 3 ? 2 : 1;
+                        result = _homeService.GetOnhandData(getSystem, transactionDetails, onHandQtyDict);
 
-
-                            return new FilterSTKCUSViewModel
-                            {
-                                PartNum = part.PartNum,
-                                Company = trans != null ? trans.Company : "SSW",
-                                TranType = trans != null ? trans.TranType : "N/A",
-                                TotalTranQty = trans != null ? trans.TotalQty : 0,
-                                OnHandQty = onHandQty,
-                                YearlyQty = part.Yearly_Qty ?? 0,
-                                CycleTime = part.Cycle_Time ?? 0,
-                                MonthsInStock = monthsInStock,
-                                PriorityRank = rank
-                            };
-                        }).ToList();
                     }
                     catch (Exception ex)
                     {
                         logService.AddLog(ex, "OnhandstkcycleFilter", "HomeController");
                     }
-
                 }
                 //stk to cus
                 else if (selectvalue == "stkToCus")
                 {
                     try
                     {
-                        result = getSystem.Select(part =>
-                        {
-                            TransactionSummary matchingTrans = transactionDetails.FirstOrDefault(t => t.PartNum == part.PartNum);
-                            int onHandQty = onHandQtyDict.ContainsKey(part.PartNum) ? onHandQtyDict[part.PartNum] : 0;
+                        result = _homeService.GetSTKCUSData(getSystem, transactionDetails, onHandQtyDict);
 
-                            decimal yearlyQty = part.Yearly_Qty ?? 0;
-                            decimal weeklyQty = yearlyQty / 52;
-                            decimal monthlyQty = yearlyQty / 12;
-                            decimal totalTranQty = matchingTrans != null ? matchingTrans.TotalQty : 0;
-
-                            decimal weeklyUsagePercentage = weeklyQty > 0 ? (totalTranQty / weeklyQty) * 100 : 0;
-
-                            int priorityRank = 5;
-                            if (weeklyUsagePercentage >= 150)
-                                priorityRank = 1;
-                            else if (weeklyUsagePercentage >= 100)
-                                priorityRank = 2;
-                            else if (weeklyUsagePercentage >= 80)
-                                priorityRank = 3;
-                            else if (weeklyUsagePercentage >= 50)
-                                priorityRank = 4;
-
-                            return new FilterSTKCUSViewModel
-                            {
-                                PartNum = part.PartNum,
-                                Company = matchingTrans != null ? matchingTrans.Company : "SSW",
-                                TranType = matchingTrans != null ? matchingTrans.TranType : "N/A",
-                                TotalTranQty = totalTranQty,
-                                OnHandQty = onHandQty,
-                                YearlyQty = yearlyQty,
-                                WeeklyQty = weeklyQty,
-                                MonthlyQty = monthlyQty,
-                                CycleTime = part.Cycle_Time ?? 0,
-                                WeeklyUsagePercentage = weeklyUsagePercentage,
-                                PriorityRank = priorityRank
-                            };
-                        })
-                       .ToList();
                     }
                     catch (Exception ex)
                     {
@@ -192,38 +132,8 @@ namespace AlertSystem.Controllers
                 {
                     try
                     {
-                        result = (
-                            from sys in getSystem
-                            join trans in transactionDetails
-                                on sys.PartNum equals trans.PartNum into transGroup
-                            from trans in transGroup.DefaultIfEmpty()
-                            let onHandQty = onHandQtyDict.ContainsKey(sys.PartNum) ? onHandQtyDict[sys.PartNum] : 0
-                            let yearlyQty = sys.Yearly_Qty ?? 0
-                            let tranQty = trans != null ? trans.TotalQty : 0
-                            let cycleTime = sys.Cycle_Time ?? 0
-                            let priorityScore = (cycleTime * 0.5m)
-                                                + (Math.Max(0, yearlyQty - onHandQty) * 0.3m)
-                                                + (tranQty * 0.2m)
-                            let priorityMonth = yearlyQty > 0 ? (onHandQty / ((double)yearlyQty / 12.0)) : 0
-                            let priorityScoreByMonth = (((double)yearlyQty * priorityMonth / 12.0) - (double)onHandQty) * (double)cycleTime
-                            let priorityRank = priorityScore >= 120 ? 1 :
-                                               priorityScore >= 90 ? 2 :
-                                               priorityScore >= 60 ? 3 :
-                                               priorityScore >= 30 ? 4 : 5
-                            select new FilterSTKCUSViewModel
-                            {
-                                Company = trans != null ? trans.Company : "SSW",
-                                PartNum = sys.PartNum,
-                                TranType = trans != null ? trans.TranType : "N/A",
-                                TotalTranQty = tranQty,
-                                OnHandQty = onHandQty,
-                                YearlyQty = yearlyQty,
-                                CycleTime = cycleTime,
-                                Priority_Score = priorityScore,
-                                prioritymonth = priorityMonth,
-                                PriorityRank = priorityRank
-                            }
-                        ).ToList();
+                        result = _homeService.GetCycletimeData(getSystem, transactionDetails, onHandQtyDict);
+
                     }
                     catch (Exception ex)
                     {
--- AlertSystem/Service/HomeService.cs
+++ AlertSystem/Service/HomeService.cs
@@ -53,5 +53,138 @@ namespace AlertSystem.Controllers
             }
             return null;
         }
+
+
+        public List<FilterSTKCUSViewModel> GetOnhandData(List<GetSystwmModel> getSystem, List<TransactionSummary> transactionDetails, Dictionary<string, int> onHandQtyDict)
+        {
+
+            List<FilterSTKCUSViewModel> result = new List<FilterSTKCUSViewModel>();
+            try
+            {
+                result = getSystem.Select((GetSystwmModel part) =>
+                {
+                    TransactionSummary trans = transactionDetails
+                        .FirstOrDefault((TransactionSummary t) => t.PartNum == part.PartNum);
+                    int onHandQty = onHandQtyDict.ContainsKey(part.PartNum) ? onHandQtyDict[part.PartNum] : 0;
+                    double monthsInStock = (part.Yearly_Qty.GetValueOrDefault() > 0) ? onHandQty / ((double)part.Yearly_Qty.GetValueOrDefault() / 12) : 0;
+                    int rank = monthsInStock >= 12 ? 5 :
+                               monthsInStock >= 9 ? 4 :
+                               monthsInStock >= 6 ? 3 :
+                               monthsInStock >= 3 ? 2 : 1;
+
+
+                    return new FilterSTKCUSViewModel
+                    {
+                        PartNum = part.PartNum,
+                        Company = trans != null ? trans.Company : "SSW",
+                        TranType = trans != null ? trans.TranType : "N/A",
+                        TotalTranQty = trans != null ? trans.TotalQty : 0,
+                        OnHandQty = onHandQty,
+                        YearlyQty = part.Yearly_Qty ?? 0,
+                        CycleTime = part.Cycle_Time ?? 0,
+                        MonthsInStock = monthsInStock,
+                        PriorityRank = rank
+                    };
+                }).ToList();
+            }
+            catch (Exception ex)
+            {
+                logService.AddLog(ex, "GetFilterData", "HomeService");
+
+            }
+
+            return result;
+
+        }
+
+        public List<FilterSTKCUSViewModel> GetSTKCUSData(List<GetSystwmModel> getSystem, List<TransactionSummary> transactionDetails, Dictionary<string, int> onHandQtyDict)
+        {
+
+            List<FilterSTKCUSViewModel> result = new List<FilterSTKCUSViewModel>();
+            try
+            {
+                result = getSystem.Select((GetSystwmModel part) =>
+                {
+                    TransactionSummary trans = transactionDetails
+                        .FirstOrDefault((TransactionSummary t) => t.PartNum == part.PartNum);
+                    int onHandQty = onHandQtyDict.ContainsKey(part.PartNum) ? onHandQtyDict[part.PartNum] : 0;
+                    double monthsInStock = (part.Yearly_Qty.GetValueOrDefault() > 0) ? onHandQty / ((double)part.Yearly_Qty.GetValueOrDefault() / 12) : 0;
+                    int rank = monthsInStock >= 12 ? 5 :
+                               monthsInStock >= 9 ? 4 :
+                               monthsInStock >= 6 ? 3 :
+                               monthsInStock >= 3 ? 2 : 1;
+
+
+                    return new FilterSTKCUSViewModel
+                    {
+                        PartNum = part.PartNum,
+                        Company = trans != null ? trans.Company : "SSW",
+                        TranType = trans != null ? trans.TranType : "N/A",
+                        TotalTranQty = trans != null ? trans.TotalQty : 0,
+                        OnHandQty = onHandQty,
+                        YearlyQty = part.Yearly_Qty ?? 0,
+                        CycleTime = part.Cycle_Time ?? 0,
+                        MonthsInStock = monthsInStock,
+                        PriorityRank = rank
+                    };
+                }).ToList();
+            }
+            catch (Exception ex)
+            {
+                logService.AddLog(ex, "GetFilterData", "HomeService");
+
+            }
+
+            return result;
+
+        }
+
+        public List<FilterSTKCUSViewModel> GetCycletimeData(List<GetSystwmModel> getSystem, List<TransactionSummary> transactionDetails, Dictionary<string, int> onHandQtyDict)
+        {
+
+            List<FilterSTKCUSViewModel> result = new List<FilterSTKCUSViewModel>();
+            try
+            {
+                result = (
+                            from sys in getSystem
+                            join trans in transactionDetails
+                                on sys.PartNum equals trans.PartNum into transGroup
+                            from trans in transGroup.DefaultIfEmpty()
+                            let onHandQty = onHandQtyDict.ContainsKey(sys.PartNum) ? onHandQtyDict[sys.PartNum] : 0
+                            let yearlyQty = sys.Yearly_Qty ?? 0
+                            let tranQty = trans != null ? trans.TotalQty : 0
+                            let cycleTime = sys.Cycle_Time ?? 0
+                            let priorityScore = (cycleTime * 0.5m)
+                                                + (Math.Max(0, yearlyQty - onHandQty) * 0.3m)
+                                                + (tranQty * 0.2m)
+                            let priorityMonth = yearlyQty > 0 ? (onHandQty / ((double)yearlyQty / 12.0)) : 0
+                            let priorityScoreByMonth = (((double)yearlyQty * priorityMonth / 12.0) - (double)onHandQty) * (double)cycleTime
+                            let priorityRank = priorityScore >= 120 ? 1 :
+                                               priorityScore >= 90 ? 2 :
+                                               priorityScore >= 60 ? 3 :
+                                               priorityScore >= 30 ? 4 : 5
+                            select new FilterSTKCUSViewModel
+                            {
+                                Company = trans != null ? trans.Company : "SSW",
+                                PartNum = sys.PartNum,
+                                TranType = trans != null ? trans.TranType : "N/A",
+                                TotalTranQty = tranQty,
+                                OnHandQty = onHandQty,
+                                YearlyQty = yearlyQty,
+                                CycleTime = cycleTime,
+                                Priority_Score = priorityScore,
+                                prioritymonth = priorityMonth,
+                                PriorityRank = priorityRank
+                            }).ToList();
+            }
+            catch (Exception ex)
+            {
+                logService.AddLog(ex, "GetFilterData", "HomeService");
+
+            }
+
+            return result;
+
+        }
     }
 }
